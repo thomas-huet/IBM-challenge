9cb70dc618aac1188161d74d454a8f286db4f227
Thomas HUET <git@thomash.fr>
2018-07-17 20:44:10 +0200

    Simpler and faster solution
    without trying to use smart maths.

--- 2018/july/main.ml
+++ 2018/july/main.ml
@@ -4,51 +4,47 @@ let product (a, b, c) = a * b * c
 
 let add (a, b, c) n = (a + n, b + n, c + n)
 
-let sqrt n =
-  let root = int_of_float (sqrt (float n)) in
-  if n = root * root then Some root
-  else None
+(* Hashtbl.find_opt is only available since ocaml 4.05 *)
+let find_opt table key = try
+  Some (Hashtbl.find table key)
+with Not_found -> None
 
-let pair s p =
-  match sqrt (s * s - 4 * p) with
-  | None -> None
-  | Some root ->
-    if (s + root) mod 2 = 1 then None
-    else Some ((s - root) / 2, (s + root) / 2)
-
-let obscure t =
-  let count s p =
-    let rec count i =
-      if i >= s then 0
-      else if p mod i = 0 then
-        match pair (s - i) (p / i) with
-        | None -> count (i + 1)
-        | Some (a, b) ->
-          if a < i then 0
-          else 1 + count (i + 1)
-      else count (i + 1)
-    in
-    count 1
-  in
-  count (sum t) (product t) > 1
-
-let rec valid n t = n = 0 || obscure t && valid (n - 1) (add t 1)
-
-let print_obscure n =
+let memo_obscure n =
+  let memo = Array.init (3 * n) Hashtbl.create in
   for i = 1 to n do
     for j = i to n do
       for k = j to n do
-        if obscure (i, j, k) then Printf.printf "%d %d %d\n" i j k
+        match find_opt memo.(i + j + k - 1) (i * j * k) with
+        | None -> Hashtbl.add memo.(i + j + k - 1) (i * j * k) false
+        | Some false -> Hashtbl.replace memo.(i + j + k - 1) (i * j * k) true
+        | Some true -> ()
+      done
+    done
+  done;
+  memo
+
+let is_obscure memo t =
+  find_opt memo.(sum t - 1) (product t) = Some true
+
+let rec valid memo n t = n = 0 || is_obscure memo t && valid memo (n - 1) (add t 1)
+
+let print_obscure n =
+  let memo = memo_obscure n in
+  for i = 1 to n do
+    for j = i to n do
+      for k = j to n do
+        if is_obscure memo (i, j, k) then Printf.printf "%d %d %d\n" i j k
         else ()
       done
     done
   done
 
 let print_valid y n =
+  let memo = memo_obscure n in
   for i = 1 to n do
     for j = i to n do
       for k = j to n do
-        if valid y (i, j, k) then Printf.printf "%d %d %d\n" i j k
+        if valid memo y (i, j, k) then Printf.printf "%d %d %d\n" i j k
         else ()
       done
     done
