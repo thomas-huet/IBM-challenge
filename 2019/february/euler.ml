ecdd418f2d04d2bf222c1e31637a03f1c6ff771c
Thomas HUET <git@thomash.fr>
2019-02-12 19:55:32 +0100

    Optimised phi

--- 2019/february/euler.ml
+++ 2019/february/euler.ml
@@ -1,42 +1,36 @@
-let factorize n =
-  let rec f n d acc =
-    if d * d > n then
-      n :: acc
-    else if n mod d = 0 then
-      f (n / d) d (d :: acc)
-    else
-      f n (d + 1) acc
+let is_prime n =
+  let rec p i =
+    i * i > n || (n mod i <> 0 && p (i + 1))
   in
-  let rec g = function
-  | [] -> []
-  | h :: t ->
-    match g t with
-    | [] -> [h, 1]
-    | (i, j) :: t ->
-      if h = i then (h, j + 1) :: t
-      else (h, 1) :: (i, j) :: t
-  in
-  if n = 1 then
-    []
-  else
-    g (f n 2 [])
+  p 2
 
-let rec pow p n =
-  if n = 0 then 1
-  else p * pow p (n - 1)
+let primes = Array.of_list (List.filter is_prime (List.init 1000000 (fun i -> i + 2)))
 
 let phi n =
-  List.fold_left (fun x (p, n) -> x * pow p (n - 1) * (p - 1)) 1 (factorize n)
+  let n = ref n in
+  let ans = ref 1 in
+  let i = ref 0 in
+  while primes.(!i) * primes.(!i) <= !n do
+    if !n mod primes.(!i) = 0 then begin
+      ans := !ans * (primes.(!i) - 1);
+      n := !n / primes.(!i);
+      while !n mod primes.(!i) = 0 do
+        ans := !ans * primes.(!i);
+        n := !n / primes.(!i)
+      done
+    end;
+    incr i
+  done;
+  if !n > 1 then
+    (!n - 1) * !ans
+  else
+    !ans
 
-let rec f n =
-  if n = 1 then Z.of_int 2
-  else Z.add (Z.of_int (phi n)) (f (n - 1))
-
-let low = Z.of_string_base 16 "1023456789abcdef"
+let low = 0x1023456789abcdef
 
 let all_digits_once n =
   n >= low && begin
-    let s = Z.format "%x" n in
+    let s = Printf.sprintf "%x" n in
     let a = Array.make 16 false in
     for i = 0 to 15 do
       match s.[i] with
@@ -47,24 +41,16 @@ let all_digits_once n =
     Array.for_all (fun b -> b) a
   end
 
-let rec find_answer n v =
-  if all_digits_once v then
-    n
-  else
-    find_answer (n + 1) (Z.add v (Z.of_int (phi (n + 1))))
-
 let () =
   (*
   let n = ref 1 in
-  let v = ref (Z.of_int 2) in
+  let v = ref 2 in
   *)
-  let n = ref 50331645 in
-  let v = ref (Z.of_string_base 16 "2bc54fa7a2f53") in
-  for e = 1 to 100 do
-    let m = pow 2 e in
-    for i = 1 to m do
-      incr n;
-      v := Z.add !v (Z.of_int (phi !n))
-    done;
-    Printf.printf "%d: %s\n%!" !n (Z.format "%x" !v)
+  let n = ref 425615482 in
+  let v = ref 0xc39f19af441b9b in
+  while true do
+    incr n;
+    v := !v + (phi !n);
+    if all_digits_once !v then
+      Printf.printf "\n=== %d: %x ===\n\n%!" !n !v
   done
